4c2fbdee8e5b641b6cec770369100165446f5bd4
unknown <hotien2107@gmail.com>
2021-10-31 16:59:57 +0700

    add home page classes

---
+++ pages/api/EnrolledClassesHomePage.js
@@ -0,0 +1,24 @@
+// /api/EnrolledClassesHomePage
+
+import { MongoClient } from "mongodb";
+
+async function handle(req, res) {
+  const url = process.env.DB_URL;
+
+  const client = await MongoClient.connect(url);
+
+  const db = client.db();
+
+  const meetupCollection = db.collection("myClasses");
+
+  const data = await meetupCollection
+    .find({ students: { name: "Nguyen Van A" } })
+    .limit(4)
+    .toArray();
+
+  client.close();
+
+  res.status(200).json(data);
+}
+
+export default handle;
---
+++ pages/api/TeachingClassesHomePage.js
@@ -0,0 +1,24 @@
+// /api/TeachingClassesHomePage
+
+import { MongoClient } from "mongodb";
+
+async function handle(req, res) {
+  const url = process.env.DB_URL;
+
+  const client = await MongoClient.connect(url);
+
+  const db = client.db();
+
+  const meetupCollection = db.collection("myClasses");
+
+  const data = await meetupCollection
+    .find({ teacher: "Nguyen Huy Khanh" })
+    .limit(4)
+    .toArray();
+
+  client.close();
+
+  res.status(200).json(data);
+}
+
+export default handle;
