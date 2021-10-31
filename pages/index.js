6f47fe4a1e03964f78729906460bf3fe474ae5ee
unknown <hotien2107@gmail.com>
2021-10-31 21:20:39 +0700

    change home page

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import Head from "next/head";
 import Link from "next/link";
-import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
@@ -18,6 +17,29 @@ export default function Home() {
         <p className={styles.description}>
           Get All Teaching Classes:{" "}
           <Link href="/api/TeachingClasses">Teaching Classes API</Link>
+        </p>
+        <p className={styles.description}>
+          Get All Enrolled Classes:{" "}
+          <Link href="/api/EnrolledClasses">Enrolled Classes API</Link>
+        </p>
+        <p className={styles.description}>
+          Get some Teaching Classes:{" "}
+          <Link href="/api/TeachingClassesHomePage">
+            Some Teaching Classes API
+          </Link>
+        </p>
+        <p className={styles.description}>
+          Get some Enrolled Classes:{" "}
+          <Link href="/api/EnrolledClassesHomePage">
+            Some Enrolled Classes API
+          </Link>
+        </p>
+        <p className={styles.description}>
+          Create a class:{" "}
+          <Link href="/api/CreateClass">Create a Class API</Link>
+        </p>
+        <p className={styles.description}>
+          Join a class: <Link href="/api/JoinClass">Join a Class API</Link>
         </p>
       </main>
     </div>
